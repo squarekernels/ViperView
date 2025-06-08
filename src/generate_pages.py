2c052a3215b0b9bc70d3fda0824f4fa6767d531a
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-06-08 20:54:48 +0500

    grab template from template file

--- src/generate_pages.py
+++ src/generate_pages.py
@@ -1,5 +1,11 @@
 import re
 
+def read_contents(file):
+    with open(file, "r")as f:
+        content = f.read()
+
+    return content 
+
 def extract_title(markdown):
     header = re.search(r"^\s*#\s+(.*)", markdown, re.MULTILINE)
     if header:
@@ -11,8 +17,10 @@ def extract_title(markdown):
 def generate_pages(from_path, template_path , dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
-    with open(from_path, "r") as from_file:
-        content = from_file.readlines()
-        print(content)
+    markdown = read_contents(from_path)
+    template = read_contents(template_path)
+
+    print("markdown: ", markdown)
+    print("template: ", template)
     
-generate_pages("content/index.md", "template.html", "public/")
+generate_pages("content/index.md", "./template.html", "public/")
