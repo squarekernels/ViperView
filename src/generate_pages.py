db3511f919864dd662dad024bb55a87ef4f13342
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-06-08 22:39:49 +0500

    bug: images not loading

--- src/generate_pages.py
+++ src/generate_pages.py
@@ -47,8 +47,8 @@ def generate_pages(basepath, from_path, template_path , dest_path):
 
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
-    template = template.replace("href=/", f"href=/{basepath}")
-    template = template.replace("src=/", f"src=/{basepath}")
+    template = template.replace("href=", f"href={basepath}")
+    template = template.replace("src=", f"src={basepath}")
 
     write_contents(dest_path, template)
 
