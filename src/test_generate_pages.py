3be5befd3c7b335253824a1ab1f54e0438af2c73
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-06-08 13:52:48 +0500

    extract title from markdown

---
+++ src/test_generate_pages.py
@@ -0,0 +1,30 @@
+import unittest
+from generate_pages import extract_title
+
+class TextExtractFiles(unittest.TestCase):
+    def test_extract_title_single_world(self):
+        markdown = """
+        # Hello
+        """
+        header = extract_title(markdown) 
+        self.assertEqual(header, "Hello")
+
+    def test_extract_title_multi_world(self):
+        markdown = """
+        #   Welcome World  
+        """
+        header = extract_title(markdown)
+        self.assertEqual(header, "Welcome World")
+
+    def test_extract_title_heading_2_before(self):
+        markdown = """
+        ## Not an H1\n# Real H1
+        """
+        header = extract_title(markdown)
+        self.assertEqual(header, "Real H1")
+
+    def test_extract_title_error(self):
+        markdown ="""
+No header in this
+"""     
+        self.assertRaises(Exception, extract_title, markdown)
