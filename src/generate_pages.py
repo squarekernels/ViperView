71f6b9ace16be0b2d324ceaf300818910f533153
hamzasafdark@gmail.com <hamzasafdark@gmail.com>
2025-06-08 20:50:30 +0500

    grab markdown from source content file

--- src/generate_pages.py
+++ src/generate_pages.py
@@ -7,3 +7,12 @@ def extract_title(markdown):
     else: 
         raise Exception("There is no header in this file")
     
+
+def generate_pages(from_path, template_path , dest_path):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    with open(from_path, "r") as from_file:
+        content = from_file.readlines()
+        print(content)
+    
+generate_pages("content/index.md", "template.html", "public/")
